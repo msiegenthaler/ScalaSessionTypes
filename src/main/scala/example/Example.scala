f8e245c30857e41797e96e03f73fb98c57a300fc
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-04-17 19:21:52 +0200

    Remove ActorIntegration, will be replaced with AkkaIntegration.

--- src/main/scala/example/Example.scala
+++ src/main/scala/example/Example.scala
@@ -23,15 +23,11 @@ object Example extends App {
     implicitly[server =:= opC.Out]
 
     printTree[client]("send/receive")
-    println(ActorIntegration[client].description)
-    println(ActorIntegration[opS.Out].description)
+    println(RequestResponse[client].description)
+    println(RequestResponse[opS.Out].description)
 
-    val actor = new ActorRef("actor-1", {
-      case a: Int => s"Hello #-$a"
-    })
-    val resp = ActorIntegration[client].exec(actor, 1234)
-    println(resp.select[String])
-    println(Handler(resp).handleTyped[String](identity).run())
+    val r = RequestResponse[client].parse("Hello").get
+    r.select[String]
   }
 
   {
@@ -42,31 +38,8 @@ object Example extends App {
     val opS = Opposite[server]
     implicitly[server =:= opC.Out]
     printTree[client]("send/receive with error handling")
-    println(ActorIntegration[client].description)
-    println(ActorIntegration[opS.Out].description)
-
-    val actor = new ActorRef("actor-2", {
-      case a: String => try {
-        a.toInt
-      } catch {
-        case e: NumberFormatException => e
-      }
-    })
-    val resp1 = ActorIntegration[client].exec(actor, "1234")
-    println(resp1.select[Int])
-    println(resp1.select[Exception])
-    val resp2 = ActorIntegration[client].exec(actor, "2a")
-    println(resp2.select[Int])
-    println(resp2.select[Exception])
-
-    println(ActorIntegration[client].handle(actor, "123").
-      handleTyped[Int](identity).
-      handleTyped[Exception](e => 0).
-      run())
-    println(ActorIntegration[client].handle(actor, "a2").
-      handleTyped[Int](identity).
-      handleTyped[Exception](e => 0).
-      run())
+    println(RequestResponse[client].description)
+    println(RequestResponse[opS.Out].description)
   }
 
   {

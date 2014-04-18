ee37c11db62e11fe6520806f3e301b50dea288e5
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-04-18 15:17:12 +0200

    Add non-compilation test.

--- src/test/scala/sst/utils/CoproductOpsSpec.scala
+++ src/test/scala/sst/utils/CoproductOpsSpec.scala
@@ -2,9 +2,12 @@ package sst.utils
 
 import org.specs2.mutable._
 import shapeless._
+import shapeless.test._
 import CoproductOps._
 
 class CoproductOpsSpec extends Specification {
+  private def assertCompiled = true must beTrue
+
   type I = Int :+: CNil
   type S = String :+: CNil
   type L = Long :+: CNil
@@ -16,70 +19,93 @@ class CoproductOpsSpec extends Specification {
   "Remove" should {
     "be I for [SI, String]" in {
       val r = Remove[SI, String]
-      implicitly[I =:= r.Out] must not beNull
+      implicitly[I =:= r.Out]
+      assertCompiled
     }
     "be S for [SI, Int]" in {
       val r = Remove[SI, Int]
-      implicitly[S =:= r.Out] must not beNull
+      implicitly[S =:= r.Out]
+      assertCompiled
     }
     "be SI for [SIL, Long" in {
       val r = Remove[SIL, Long]
-      implicitly[SI =:= r.Out] must not beNull
+      implicitly[SI =:= r.Out]
+      assertCompiled
     }
     "be SL for [SIL, Int]" in {
       val r = Remove[SIL, Int]
-      implicitly[SL =:= r.Out] must not beNull
+      implicitly[SL =:= r.Out]
+      assertCompiled
     }
     "be IL for [SIL, S]" in {
       val r = Remove[SIL, String]
-      implicitly[IL =:= r.Out] must not beNull
+      implicitly[IL =:= r.Out]
+      assertCompiled
+    }
+
+    "not compile for [I, S]" in {
+      illTyped("Remove[L,S]")
+      assertCompiled
+    }
+    "not compile for [IL, S]" in {
+      illTyped("Remove[IL,S]")
+      assertCompiled
     }
   }
 
   "Contains" should {
     "be True for [SI, String]" in {
       val r = Contains[SI, String]
-      implicitly[True =:= r.Out] must not beNull
+      implicitly[True =:= r.Out]
+      assertCompiled
     }
     "be True in [SI, Int]" in {
       val r = Contains[SI, Int]
-      implicitly[True =:= r.Out] must not beNull
+      implicitly[True =:= r.Out]
+      assertCompiled
     }
     "be False in [SI, Long" in {
       val r = Contains[SI, Long]
-      implicitly[False =:= r.Out] must not beNull
-
+      implicitly[False =:= r.Out]
+      assertCompiled
     }
   }
 
   "Concat" should {
     "be I for [I, CNil]" in {
       val r = Concat[I, CNil]
-      implicitly[I =:= r.Out] must not beNull
+      implicitly[I =:= r.Out]
+      assertCompiled
     }
     "be SI for [SI, CNil]" in {
       val r = Concat[SI, CNil]
-      implicitly[SI =:= r.Out] must not beNull
+      implicitly[SI =:= r.Out]
+      assertCompiled
     }
     "be I for [CNil and I]" in {
       val r = Concat[CNil, I]
-      implicitly[I =:= r.Out] must not beNull
+      implicitly[I =:= r.Out]
+      assertCompiled
     }
     "be IL for [I and L]" in {
       val r = Concat[I, L]
-      implicitly[IL =:= r.Out] must not beNull
+      implicitly[IL =:= r.Out]
+      assertCompiled
     }
     "be SIL for [SI, L]" in {
       val r = Concat[SI, L]
-      implicitly[SIL =:= r.Out] must not beNull
+      implicitly[SIL =:= r.Out]
+      assertCompiled
     }
     "be SIL for [S, IL]" in {
       val r = Concat[S, IL]
-      implicitly[SIL =:= r.Out] must not beNull
+      implicitly[SIL =:= r.Out]
+      assertCompiled
     }
     "be Double :+: SIL for [Double :+: S, IL]" in {
       val r = Concat[Double :+: S, IL]
-      implicitly[(Double :+: SIL) =:= r.Out] must not beNull
+      implicitly[(Double :+: SIL) =:= r.Out]
+      assertCompiled
     }
   }
 }

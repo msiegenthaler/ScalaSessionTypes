cdfa7f9503db86774607f238c94dfa3a728f037c
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-04-21 23:52:05 +0200

    Add subscription.

---
+++ src/main/scala/sst/Subscription.scala
@@ -0,0 +1,27 @@
+package sst
+
+import scala.language.implicitConversions
+import shapeless._
+import scala.reflect.ClassTag
+
+sealed trait Subscription[A <: Action] {
+  type Setup
+  type Message <: Coproduct
+  def parse: PartialFunction[Any, Message]
+  def description: String
+}
+
+object Subscription {
+  def apply[A <: Action](implicit s: Subscription[A]): Aux[A, s.Setup, s.Message] = s
+
+  type Aux[A <: Action, S, M <: Coproduct] = Subscription[A] {type Setup = S; type Message = M}
+  implicit def sendRepeatedReceive[S: ClassTag, R <: Action](implicit r: Response[R]): Aux[Then[Send[S], Repeat[R]], S, r.Out] = new Subscription[Then[Send[S], Repeat[R]]] {
+    type Setup = S
+    type Message = r.Out
+    def description = implicitly[ClassTag[S]].toString + " subscribes to " + r.description
+    def parse = {
+      case msg if r.parse(msg).isDefined =>
+        r.parse(msg).get
+    }
+  }
+}
---
+++ src/test/scala/sst/SubscriptionSpec.scala
@@ -0,0 +1,59 @@
+package sst
+
+import shapeless._
+import org.specs2.mutable._
+
+class SubscriptionSpec extends Specification {
+  private def isOk() = true must beTrue
+
+  "Subscription" should {
+    "parse ![String] :>: Repeat[?[Int]] into Setup=String, Message=Int :+: CNil" in {
+      val s = Subscription[![String] :>: Repeat[?[Int]]]
+      implicitly[s.Setup =:= String]
+      implicitly[s.Message =:= (Int :+: CNil)]
+      isOk()
+    }
+    "parse ![Int] :>: Repeat[?[String] :&: ?[Long]] into Setup=Int, Message=String :+: Long :+: CNil" in {
+      val s = Subscription[![Int] :>: Repeat[?[String] :&: ?[Long]]]
+      implicitly[s.Setup =:= Int]
+      implicitly[s.Message =:= (String :+: Long :+: CNil)]
+      isOk()
+    }
+  }
+
+  "Subscription[![Int] :>: Repeat[?[String] :&: ?[Long]]].parse" should {
+    val s = Subscription[![Int] :>: Repeat[?[String] :&: ?[Long]]]
+    "be defined for Strings" in {
+      s.parse.isDefinedAt("hello") must beTrue
+      s.parse.isDefinedAt("") must beTrue
+      s.parse.isDefinedAt("Mario") must beTrue
+    }
+    "be defined for Longs" in {
+      s.parse.isDefinedAt(0L) must beTrue
+      s.parse.isDefinedAt(1L) must beTrue
+      s.parse.isDefinedAt(-1L) must beTrue
+      s.parse.isDefinedAt(2L) must beTrue
+      s.parse.isDefinedAt(1231341113L) must beTrue
+    }
+    "be undefined for Ints" in {
+      s.parse.isDefinedAt(0) must beFalse
+      s.parse.isDefinedAt(1) must beFalse
+    }
+
+    "return Coproduct(\"hello\") for String hello" in {
+      s.parse("hello") must_== Coproduct[String :+: Long :+: CNil]("hello")
+    }
+    "return Coproduct(2L) for Long 2" in {
+      s.parse(2L) must_== Coproduct[String :+: Long :+: CNil](2L)
+    }
+  }
+
+  "Subscription.description" should {
+    "be 'Int subscribes to java.lang.String' for ![Int] :>: Repeat[?[String]]" in {
+      Subscription[![Int] :>: Repeat[?[String]]].description must_== "Int subscribes to java.lang.String"
+    }
+    "be 'Int subscribes to java.lang.String or Long' for ![Int] :>: Repeat[?[String] :&: ?[Long]]" in {
+      Subscription[![Int] :>: Repeat[?[String] :&: ?[Long]]].description must_== "Int subscribes to java.lang.String or Long"
+    }
+  }
+}

6c1905708133484424f3e90137ae1118c09cfcfe
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-04-21 22:28:16 +0200

    Shorten example.

--- src/main/scala/example/Example.scala
+++ src/main/scala/example/Example.scala
@@ -1,7 +1,7 @@
 package example
 
 import scala.language.reflectiveCalls
-import scala.concurrent._
+import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits._
 import scala.concurrent.duration._
 import akka.actor._
@@ -11,138 +11,27 @@ import sst.Description._
 import sst.akka._
 
 object Example extends App {
-  def printTree[A <: Action : TS](name: String) = {
-    println()
-    println(s"*** $name ***")
-    println(Description.asTree[A])
-  }
-
-  {
-    type client = ![Int] :>: ?[String]
-    type server = ?[Int] :>: ![String]
-    Opposite.is[client, server]
-    val opC = Opposite[client]
-    val opS = Opposite[server]
-    implicitly[server =:= opC.Out]
-
-    printTree[client]("send/receive")
-    println(RequestResponse[client].description)
-    println(RequestResponse[opS.Out].description)
-
-    val r = RequestResponse[client].parse("Hello").get
-    r.select[String]
-  }
-
-  {
-    type client = ![String] :>: (?[Int] :&: ?[Exception])
-    type server = ?[String] :>: (![Int] :@: ![Exception])
-    Opposite.is[client, server]
-    val opC = Opposite[client]
-    val opS = Opposite[server]
-    implicitly[server =:= opC.Out]
-    printTree[client]("send/receive with error handling")
-    println(RequestResponse[client].description)
-    println(RequestResponse[opS.Out].description)
-  }
-
-  {
-    type client = Repeat[![String] :>: (?[Int] :&: ?[Exception])]
-    type server = Repeat[?[String] :>: (![Int] :@: ![Exception])]
-    Opposite.is[client, server]
-    val opC = Opposite[client]
-    val opS = Opposite[server]
-    implicitly[server =:= opC.Out]
-    printTree[client]("repeated send/receive with error handling")
-    println(RequestResponse[client].description)
-    println(RequestResponse[opS.Out].description)
-  }
-
-  {
-    type client = ![String] :>: (?[Int] :&: ?[String] :&: ?[Exception])
-    type server = ?[String] :>: (![Int] :@: ![String] :@: ![Exception])
-    Opposite.is[client, server]
-    val opC = Opposite[client]
-    val opS = Opposite[server]
-    implicitly[server =:= opC.Out]
-    printTree[client]("send/receive with 3 elements")
-    println(RequestResponse[client].description)
-    println(RequestResponse[opS.Out].description)
-  }
-
-  {
-    type client = ![String] :>: (?[Int] :&: ?[Long] :&: ?[String] :&: ?[Exception])
-    type server = ?[String] :>: (![Int] :@: ![Long] :@: ![String] :@: ![Exception])
-    Opposite.is[client, server]
-    val opC = Opposite[client]
-    val opS = Opposite[server]
-    implicitly[server =:= opC.Out]
-    printTree[client]("send/receive with 4 elements")
-    println(RequestResponse[client].description)
-    println(RequestResponse[opS.Out].description)
-  }
-
-  {
-    type client = ![String] :>: (((?[Int] :&: ?[Long]) :&: ?[String]) :&: ?[Exception])
-    type server = ?[String] :>: (((![Int] :@: ![Long]) :@: ![String]) :@: ![Exception])
-    Opposite.is[client, server]
-    val opC = Opposite[client]
-    val opS = Opposite[server]
-    implicitly[server =:= opC.Out]
-    printTree[client]("send/receive with 4 elements left-bound")
-    println(RequestResponse[client].description)
-    println(RequestResponse[opS.Out].description)
-  }
-
-  /*
-  //Not yet supported
-  {
-    type client = ![String] :>: ((?[Int] :&: ?[Long]) :&: (?[String] :&: ?[Exception]))
-    type server = ?[String] :>: ((![Int] :@: ![Long]) :@: (![String] :@: ![Exception]))
-    Opposite.is[client, server]
-    val opC = Opposite[client]
-    val opS = Opposite[server]
-    implicitly[server =:= opC.Out]
-    printTree[client]("send/receive with 4 elements grouped")
-    println(RequestResponse[client].description)
-    println(RequestResponse[opS.Out].description)
-  }
-  */
-
-  {
-    type client = Repeat[![String] :>: ?[Int]]
-    type server = Repeat[?[String] :>: ![Int]]
-    Opposite.is[client, server]
-    val opC = Opposite[client]
-    implicitly[server =:= opC.Out]
-    printTree[client]("send/receive in loop")
-  }
-
-  {
-    type client = Repeat[(![String] :>: ?[Int]) :@: (![Unit] :>: Break)]
-    type server = Repeat[(?[String] :>: ![Int]) :&: (?[Unit] :>: Break)]
-    Opposite.is[client, server]
-    val opC = Opposite[client]
-    implicitly[server =:= opC.Out]
-    printTree[client]("send/receive in loop with break")
-  }
-
-  {
-    val x = send[Int].
-      send[Long].receiveAnyOf2[String, Exception].answer[Unit].
-      chooseFrom(
-        send[Int].receive[String],
-        send[String].receive[Int])
-    Opposite[x.Type]
-  }
-
   val akka = ActorSystem("Example")
   implicit val defaultTimeout: Timeout = 1.second
 
-  def actors() = {
-    val cn = send[String].receiveAnyOf[Int, Exception]
-    type ConvertNumber = cn.Type
 
-    class MyActor extends Actor {
+  type ConvertNumberClient = ![String] :>: (?[Int] :&: ?[Exception])
+  val server = Opposite[ConvertNumberClient]
+  type ConvertNumberServer = server.Out
+
+
+  def descriptions() = {
+    println("client:")
+    println(Description.asTree[ConvertNumberClient])
+    println(RequestResponse[ConvertNumberClient].description)
+    println()
+    println("server:")
+    println(Description.asTree[ConvertNumberServer])
+    println()
+  }
+
+  def actors() = {
+    class ConverterActor extends Actor {
       def receive = {
         case a: String =>
           val result = try {
@@ -153,10 +42,10 @@ object Example extends App {
           sender ! result
       }
     }
-    val actor: ActorRef = akka.actorOf(Props(new MyActor))
+    val actor: ActorRef = akka.actorOf(Props(new ConverterActor))
 
     println("Converting numbers")
-    val convertNumber = actor.ask[ConvertNumber]
+    val convertNumber = actor.ask[ConvertNumberClient]
       .handle[Int](identity)
       .handle[Exception](_ => -1)
 
@@ -167,6 +56,8 @@ object Example extends App {
     println(s"Converting a2 yields $r2")
 
   }
+
+  descriptions()
   actors()
 
   akka.shutdown()

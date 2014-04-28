4dbe8f301d9dbd0d73e2c5eeb311e5785889e765
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-04-28 21:19:54 +0200

    Opposite returns instance.

--- src/test/scala/sst/OppositeSpec.scala
+++ src/test/scala/sst/OppositeSpec.scala
@@ -10,82 +10,82 @@ class OppositeSpec extends Specification {
     "be Receive[String] for Send[String]" in {
       type In = ![String]
       type Op = ?[String]
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
     "be Send[String] for Receive[String]" in {
       type In = ?[String]
       type Op = ![String]
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
 
     "be (![String] :>: ?[Int]) for (?[String] :>: ![Int]" in {
       type In = ?[String] :>: ![Int]
       type Op = ![String] :>: ?[Int]
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
     "be (?[String] :>: ![Int]) for (![String] :>: ?[Int]" in {
       type In = ![String] :>: ?[Int]
       type Op = ?[String] :>: ![Int]
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
 
     "be Choice[![String],![Int]] for AnyOf[?[String],?[Int]" in {
       type In = ?[String] :&: ?[Int]
       type Op = ![String] :@: ![Int]
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
     "be AnyOf[?[String],?[Int] for Choice[![String],![Int]]" in {
       type In = ![String] :@: ![Int]
       type Op = ?[String] :&: ?[Int]
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
     "be AnyOf[?[String],AnyOf[?[Int],?[Long]] for Choice[![String],Choice[![Int],![Long]]" in {
       type In = ![String] :@: ![Int] :@: ![Long]
       type Op = ?[String] :&: ?[Int] :&: ?[Long]
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
     "be Choice[![String],Choice[![Int],![Long]] for AnyOf[?[String],AnyOf[?[Int],?[Long]]" in {
       type In = ?[String] :&: ?[Int] :&: ?[Long]
       type Op = ![String] :@: ![Int] :@: ![Long]
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
 
     "be Repeat[![String]] for Repeat[?[String]]" in {
       type In = Repeat[?[String]]
       type Op = Repeat[![String]]
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
     "be Break for Break" in {
       type In = Break
       type Op = Break
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
     "be (![String] :>: Break) for (?[String] :>: Break)" in {
       type In = ?[String] :>: Break
       type Op = ![String] :>: Break
-      val op = Opposite[In]
-      implicitly[Op =:= op.Out]
-      assertCompiled
+      val op: Op = Opposite[In]
+      implicitly[Op =:= op.Type]
+      op must not beNull
     }
   }
 
@@ -132,29 +132,29 @@ class OppositeSpec extends Specification {
     "be original for Receive[String]" in {
       type T = ?[String]
       val r = Opposite[T]
-      val o = Opposite[r.Out]
-      implicitly[T =:= o.Out]
+      val o = Opposite[r.Type]
+      implicitly[T =:= o.Type]
       assertCompiled
     }
     "be original for Send[String]" in {
       type T = ![String]
       val r = Opposite[T]
-      val o = Opposite[r.Out]
-      implicitly[T =:= o.Out]
+      val o = Opposite[r.Type]
+      implicitly[T =:= o.Type]
       assertCompiled
     }
     "be original for ![String] :>: ?[Int]" in {
       type T = ![String] :>: ?[Int]
       val r = Opposite[T]
-      val o = Opposite[r.Out]
-      implicitly[T =:= o.Out]
+      val o = Opposite[r.Type]
+      implicitly[T =:= o.Type]
       assertCompiled
     }
     "be original for ?[String] :>: (![Int] :@: ![Long] :@: ![Double])" in {
       type T = ?[String] :>: (![Int] :@: ![Long] :@: ![Double])
       val r = Opposite[T]
-      val o = Opposite[r.Out]
-      implicitly[T =:= o.Out]
+      val o = Opposite[r.Type]
+      implicitly[T =:= o.Type]
       assertCompiled
     }
   }

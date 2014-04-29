6b4293e965a87b59c7e5f6c7b43278479bb76acf
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2014-04-29 22:25:42 +0200

    Update KvStoreSpec to new akka ask pattern.

--- src/test/scala/example/kvstore/KvStoreSpec.scala
+++ src/test/scala/example/kvstore/KvStoreSpec.scala
@@ -5,9 +5,11 @@ import scala.concurrent._
 import akka.actor._
 import akka.testkit._
 import akka.util._
+import shapeless._
 import org.specs2.mutable._
 import org.specs2.specification.Context
 import sst.akka._
+import sst.utils.CoproductFold
 import Replica._
 
 class KvStoreSpec extends Specification {
@@ -22,9 +24,7 @@ class KvStoreSpec extends Specification {
     val persistence = Persistence.props(true)
     val nodes = (1 to 3).map(i => system.actorOf(Replica.props(arbiter, persistence), s"node-$i"))
     def leader = {
-      val f = arbiter.ask[Arbiter.getLeader.Type].
-        handle[Option[ActorRef]](identity).
-        send(Arbiter.GetLeader)
+      val f = Arbiter.getLeader(arbiter) ? Arbiter.GetLeader
       Await.result(f, timeout.duration).
         getOrElse(throw new RuntimeException("no leader"))
     }
@@ -32,40 +32,29 @@ class KvStoreSpec extends Specification {
   }
 
   "KV-Store" should {
+    val ackFailed = CoproductFold[OperationAck :+: OperationFailed :+: CNil]
+      .fold[OperationAck](_ => true)
+      .fold[OperationFailed](_ => false)
     "successfully store a value" in new kvstore {
-      val f = leader.ask[insert.Type].
-        handle[OperationAck](a => a.id).
-        handle[OperationFailed](_ => -1).
-        send(Insert("key", "value", 1))
-
-      valueOf(f) must_== 1
+      val f = insert(leader).map(ackFailed) ? Insert("key", "value", 1)
+      valueOf(f) must beTrue
       system.shutdown()
     }
     "deliver a stored value at leader" in new kvstore {
-      val i1 = leader.ask[insert.Type].
-        handle[OperationAck](a => a.id).
-        handle[OperationFailed](_ => -1).
-        send(Insert("key1", "my value", 1))
-      valueOf(i1) must_== 1
+      val i1 = insert(leader).map(ackFailed) ? Insert("key1", "my value", 1)
+      valueOf(i1) must beTrue
 
-      val g1 = leader.ask[get.Type].
-        handle[GetResult](_.valueOption).
-        send(Get("key1", 2))
+      val g1 = get(leader).map(_.valueOption) ? Get("key1", 2)
       valueOf(g1) must_== Some("my value")
 
       system.shutdown()
     }
     "deliver a stored value at replica" in new kvstore {
-      val i1 = leader.ask[insert.Type].
-        handle[OperationAck](a => a.id).
-        handle[OperationFailed](_ => -1).
-        send(Insert("key1", "my value", 1))
-      valueOf(i1) must_== 1
+      val i1 = insert(leader).map(ackFailed) ? Insert("key1", "my value", 1)
+      valueOf(i1) must beTrue
 
-      val g1 = replicas.head.ask[get.Type].
-        handle[GetResult](_.valueOption).
-        send(Get("key1", 2))
-      valueOf(g1) must_== Some("my value")
+      val g1 = get(replicas.head) ? Get("key1", 2)
+      valueOf(g1.map(_.valueOption)) must_== Some("my value")
 
       system.shutdown()
     }
